812c351a401475bd25f8e42256cc5ff4cd56bf4f
decolla <pedro.varise@gmail.com>
2025-04-15 10:33:29 -0300

    Update Tipos.hs

--- Tipos.hs
+++ Tipos.hs
@@ -1,7 +1,11 @@
 import Data.Time.Calendar
+
 data Status = Pendente | Concluida deriving (Show, Eq)
+
 data Prioridade = Baixa | Media | Alta deriving (Show, Eq, Ord)
+
 data Categoria = Trabalho | Estudos | Pessoal | Outro deriving (Show, Eq)
+
 data Tarefa = Tarefa
     { idTarefa :: Int
     , descricao :: String
