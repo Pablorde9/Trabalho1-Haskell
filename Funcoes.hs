2f63e141fc48326cd0a2b6b72aefeafcc0f9c214
decolla <pedro.varise@gmail.com>
2025-04-15 11:09:22 -0300

    Update Funcoes.hs

--- Funcoes.hs
+++ Funcoes.hs
@@ -3,6 +3,7 @@ module Funcoes where
 import Tipos
 
 adicionarTarefa :: Tarefa -> [Tarefa] -> [Tarefa]
+adicionarTarefa tarefa lista = tarefa : lista --adicionar depois uma validação para id
 
 removerTarefa :: Int -> [Tarefa] -> [Tarefa]
 
