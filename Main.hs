869ebefe0ca3b84306fdc0f30caa0b78bb1f966d
Coelho09 <rrapozao7@live.com>
2025-04-21 12:12:08 -0300

    Update Main.hs
    To mexando com o Main

--- Main.hs
+++ Main.hs
@@ -1,1 +1,11 @@
+module Main (main) where
 
+import Funcoes
+
+main :: IO ()
+main = do
+          putStrLn "Bem vindo ao Sistema de Gerenciamento de Tarefas!"
+          putStrLn "Digite o que deseja fazer:"
+          putStrLn "1. Modificar a Lista de Tarefas."
+          putStrLn "2. Verificar alguma Informação na lista."
+          putStrLn "3. Sair."
