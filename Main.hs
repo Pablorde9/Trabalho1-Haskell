db5b73331007d3f5a761f837a40dea9d2cbad42c
decolla <pedro.varise@gmail.com>
2025-04-22 12:09:17 -0300

    Mudei o cabeçalho

--- Main.hs
+++ Main.hs
@@ -1,6 +1,14 @@
 module Main (main) where
 
+import Tipos
 import Funcoes
+import Persistencia
+import System.IO
+import Data.Time.Calendar (Day, fromGregorian)
+import Data.Time.Clock (getCurrentTime, utctDay)
+import System.Directory (doesFileExist)
+import Control.Monad (when)
+import Text.Read (readMaybe)
 import System.Exit (exitSuccess)
 
 main :: IO ()
